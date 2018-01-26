958ec2578482a9237791fcf37f1e55125d34c9e7
prettydiff <info@prettydiff.com>
2018-01-25 19:41:49 -0500

    adding Colorize

--- funky.ps1
+++ funky.ps1
@@ -1,8 +1,10 @@
-function Get-Funky ([string]$Text, [int]$Iteration) {
-    [char[]]$list = $Text.toCharArray()
-    [string[]]$color = [string]"Black",[string]"DarkBlue",[string]"DarkGreen",[string]"DarkCyan",[string]"DarkRed",[string]"DarkMagenta",[string]"DarkYellow",[string]"Gray",[string]"DarkGray",[string]"Blue",[string]"Green",[string]"Cyan",[string]"Red",[string]"Magenta",[string]"Yellow",[string]"White"
+function Get-Funky ([Parameter(Mandatory=$true)][string]$Text, [int]$Iteration, [switch]$Colorize) {
+    [string]$script = $Text -replace "^\{" -replace "\}$"
+    [string]$textchars = If ($Text[0] -eq "{" -and $Text[$Text.length - 1] -eq "}") {Invoke-Expression $script} Else {$Text}
+    [char[]]$list = $textchars.toCharArray()
+    [string[]]$colors = [string]"Black",[string]"DarkBlue",[string]"DarkGreen",[string]"DarkCyan",[string]"DarkRed",[string]"DarkMagenta",[string]"DarkYellow",[string]"Gray",[string]"DarkGray",[string]"Blue",[string]"Green",[string]"Cyan",[string]"Red",[string]"Magenta",[string]"Yellow",[string]"White"
     [int]$len = $list.length
-    [int]$clen = $color.length
+    [int]$clen = $colors.length
     [int]$a = 0
     [int]$b = 0
     [int]$c = Get-Random -Maximum ($clen - 1)
@@ -10,17 +12,21 @@ function Get-Funky ([string]$Text, [int]$Iteration) {
     if ($len -lt 1) {
         return "The a string value is required for the -Text parameter."
     }
-    do {
-        $b = 0
+    if ($Colorize -eq $True) {
         do {
-            Write-Host $list[$b] -ForeGroundColor $color[$c] -NoNewLine
-            $b = $b + 1
-            $c = $c + 1
-            if ($c -eq $clen) {
-                $c = 0
-            }
-        } while ($b -lt $len)
-        Write-Host "`r`n" -NoNewLine
-        $a = $a + 1
-    } while ($a -lt $iter)
+            $b = 0
+            do {
+                Write-Host $list[$b] -ForeGroundColor $colors[$c] -NoNewLine
+                $b = $b + 1
+                $c = $c + 1
+                if ($c -eq $clen) {
+                    $c = 0
+                }
+            } while ($b -lt $len)
+            Write-Host "`r`n" -NoNewLine
+            $a = $a + 1
+        } while ($a -lt $iter)
+    } else {
+        Write-Host $textchars
+    }
 }
