2f9c05ff01578be21faa18b5b90878b0fc2d0146
prettydiff <info@prettydiff.com>
2018-01-26 14:30:32 -0500

    finished product from class colaboration

--- funky.ps1
+++ funky.ps1
@@ -1,3 +1,4 @@
+# Cheney - String coercion and colorful text output
 function Get-Funky ([Parameter(Mandatory=$true)][string]$Text,[int]$Iteration,[switch]$Matrix,[switch]$Colorize,[switch]$About) {
     <#
         .DESCRIPTION
@@ -29,7 +30,7 @@ function Get-Funky ([Parameter(Mandatory=$true)][string]$Text,[int]$Iteration,[s
         https://github.com/prettydiff/powershell
     #>
     [string]$script = $Text -replace "^\{" -replace "\}$"
-    [string]$textchars = If ($Text[0] -eq "{" -and $Text[$Text.length - 1] -eq "}") {Invoke-Expression $script} Else {$Text}
+    [string]$textchars = If ($Text[0] -eq "{" -and $Text[$Text.length - 1] -eq "}") {Invoke-Expression -Command $script} Else {$Text}
     [string[]]$colors = [string]"Black",[string]"DarkBlue",[string]"DarkGreen",[string]"DarkCyan",[string]"DarkRed",[string]"DarkMagenta",[string]"DarkYellow",[string]"Gray",[string]"DarkGray",[string]"Blue",[string]"Green",[string]"Cyan",[string]"Red",[string]"Magenta",[string]"Yellow",[string]"White"
     function colorate {
         [char[]]$list = $textchars.toCharArray()
@@ -77,3 +78,50 @@ function Get-Funky ([Parameter(Mandatory=$true)][string]$Text,[int]$Iteration,[s
     }
 }
 New-Alias -Name funkit -Value Get-Funky -Description "Just go funkit" -Force
+
+# Jumper - open cd drive code
+function cdopen {
+    $Diskmaster = New-Object -ComObject IMAPI2.MsftDiscMaster2
+    $DiskRecorder = New-Object -ComObject IMAPI2.MsftDiscRecorder2
+    $DiskRecorder.InitializeDiscRecorder($DiskMaster)
+    $DiskRecorder.EjectMedia()
+}
+
+# Amboy - Application architecture
+function Invoke-Matrix {
+
+    # Remove prior written directory, so that there aren't errors later
+    $pathy = 'c:\Users\Student\Desktop\Script'
+    $pathtest = Test-Path $pathy
+    If ($pathtest -eq $True) {
+        rm -recurse -force $pathy
+    }
+
+    # Amboy - Prompt for user input
+    $WhatsYourName = Read-Host -Prompt 'Input your name'
+    $NoADC = (Get-NetNeighbor).Count
+    $NoADU = $NoADC - (Get-NetNeighbor | ? State -eq Unreachable).Count
+    $Today = Get-Date
+    Write-Host -ForeGroundColor "Green" "Hello $WhatsYourName as of $Today you have $NoADC neighbors and $NoADU are reachable"
+    
+    # Oliver - Speech
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.speak("Hello $WhatsYourName as of $Today you have $NoADC neighbors and $NoADU are reachable")
+    Write-Host "You should have chosen the blue pill..."
+    
+    # Jumper - Open the CD drive
+    Start-Sleep -s 2
+    Write-Host "But now Morpheus has haunted your CD drive..."
+    cdopen       # function above
+
+    # Oliver - Writing to the file system
+    $Output = (Read-Host -Prompt 'Do you want to save IP addresses to a file? Y/N').toLower()
+    If ($Output[0] -eq 'y') {
+        New-Item -Path $pathy -ItemType dir
+	Get-NetNeighbor | Select -Property IPAddress | Out-File 'c:\Users\Student\Desktop\Script\IPAddresses.txt'
+    }
+
+    # Cheney - Matrix scrolling
+    $x = ps|ConvertTo-HTML;Get-Funky -Text "$x" -Matrix -About -Iteration 1
+}
